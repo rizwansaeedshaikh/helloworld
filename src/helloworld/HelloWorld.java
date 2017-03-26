f4d4bfea80261f3572b3ad6c2216d0dd4e55819c
Rizwan Saeed Shaikh <rizwan.saeed.shaikh@aonhewitt.com>
2017-03-26 10:49:29 +0530

    Took this opportunity to thank the Almighty Allah.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
+		
+		System.out.println("How blessed we are. Let us take this opportunity to thank the Almighty Allah!");
 	}
 }
